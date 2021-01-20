04ac931b78b87c6c28626ce250bc2a9dd3fcca8d
Henrique Barbosa <henriqbarbosa@icloud.com>
2021-01-20 16:04:56 -0300

    Adicionar esquema de aula

--- api/models/course.js
+++ api/models/course.js
@@ -1,3 +1,13 @@
-const Course = {};
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const courseSchema = new Schema({
+    title: String,
+    grade: Number,
+    professor: String,
+})
+
+const Course = mongoose.model('Course', courseSchema);
 
 export default Course;
