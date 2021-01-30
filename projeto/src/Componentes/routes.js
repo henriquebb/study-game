5460c7c8eb929f6177c17445a455691f71acc7bf
MaMiotto <matheusgveloso@hotmail.com>
2021-01-30 18:49:29 -0300

    Interface Mínima para Cadastro de Semestre

--- projeto/src/Componentes/routes.js
+++ projeto/src/Componentes/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 
 import Aulas from '../Pages/aulas.js';
+import CadastraSemestre from '../Pages/cadastraSemestre.js';
 
 export default function Routes(){
     return (
         <BrowserRouter>
-            <Route exact path="/" component={Aulas}/>
+            <Route exact path="/aulas" component={Aulas}/>
+            <Route exact path="/novo-semestre" component={CadastraSemestre}/>
         </BrowserRouter>
     );
 }
