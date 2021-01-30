327f7af03b926a1e4a7432629d30869294681ad1
Henrique Barbosa <henriqbarbosa@icloud.com>
2021-01-30 13:29:26 -0300

    Adicionar esquema de loja

---
+++ api/models/store.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+import Item from 'item';
+
+const { Schema } = mongoose;
+
+const storeSchema = new Schema({
+    items: [Item]
+})
+
+const Store = mongoose.model('Store', storeSchema);
+
+export default Store;
