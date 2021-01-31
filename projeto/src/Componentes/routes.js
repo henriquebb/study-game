10b708c758b186773fc1e1f07b1ca8322c22758f
MaMiotto <matheusgveloso@hotmail.com>
2021-01-31 00:48:47 -0300

    Criando Tela de Edição de Semestre

--- projeto/src/Componentes/routes.js
+++ projeto/src/Componentes/routes.js
@@ -3,12 +3,14 @@ import {BrowserRouter, Route} from 'react-router-dom';
 
 import Aulas from '../Pages/aulas.js';
 import CadastraSemestre from '../Pages/cadastraSemestre.js';
+import EditaSemestre from '../Pages/editaSemestre.js'
 
 export default function Routes(){
     return (
         <BrowserRouter>
             <Route exact path="/aulas" component={Aulas}/>
             <Route exact path="/novo-semestre" component={CadastraSemestre}/>
+            <Route exact path="/edita-semestre" component={EditaSemestre}/>
         </BrowserRouter>
     );
 }
---
+++ projeto/src/Pages/editaSemestre.js
@@ -0,0 +1,68 @@
+import '../Estilos/geral.css'
+import logo from '../Assets/logo.png'
+
+import React, {Component} from 'react';
+import { IconContext } from "react-icons";
+import { VscAccount, VscCalendar, VscEdit } from "react-icons/vsc";
+import { RiShoppingBag2Line, RiSwordLine, RiLogoutCircleRLine } from "react-icons/ri";
+
+class EditaSemestre extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            nomeAntes:props.history.location.state,
+            novoNome: props.history.location.state
+        };
+
+        this.handleNameChange = this.handleNameChange.bind(this);
+    }
+
+    handleNameChange(event){
+        this.setState({novoNome: event.target.value});
+    }
+
+    render(){
+        return (
+            <div className="pagina">
+                <link rel="stylesheet" href= "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"></link>
+                <div className="cabecalho"> {/* Começo do Cabeçalho */}
+                    <div className="menu1"> {/* Início Logo + Menu */}
+                    <IconContext.Provider
+                        value={{ color: 'black', height: '40%' }}
+                        >
+                    <img className="img-logo" src={logo}/>
+                    <VscAccount/>
+                    <a href="/perfil">Perfil</a>
+                    <VscEdit/>
+                    <a href="/aulas">Aulas</a>
+                    <VscCalendar/>
+                    <a href="/calendario">Calendário</a>
+                    <RiSwordLine/>
+                    <a href="/missoes">Missões</a>
+                    <RiShoppingBag2Line/>
+                    <a href="/loja">Loja</a>
+                    </IconContext.Provider>
+                </div> {/* Fim Logo + Menu */}
+                    <div className="header-right"> {/* Início Logout */}
+                        <RiLogoutCircleRLine/>
+                        <a href="">Logout</a>
+                    </div> {/* Fim Logout */}
+                </div> {/* Fim do Cabeçalho */}
+
+                <div>
+                    <h4>Editar Semestre</h4>
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                            Nome:
+                            <input type="text" value={this.state.novoNome} onChange={this.handleNameChange}  />
+                        </label>
+                        <input type="submit" value="Salvar" />
+                    </form>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default EditaSemestre;
