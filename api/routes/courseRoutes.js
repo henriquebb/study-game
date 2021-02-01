a14ff6cf999623002632542aaf7180a0ed9e7ad5
Henrique Barbosa <henriqbarbosa@icloud.com>
2021-02-01 12:08:30 -0300

    Hotfix notas de aula

--- api/routes/courseRoutes.js
+++ api/routes/courseRoutes.js
@@ -1,8 +1,8 @@
 import Router from 'express';
-import { addNotesToCourse, createCourse } from '../controllers/course.js';
+import { addNoteToCourse, createCourse } from '../controllers/course.js';
 
 const courseRoute = Router();
 courseRoute.post("/courses", createCourse);
-courseRoute.patch("/courses/:id/add-notes", addNotesToCourse);
+courseRoute.patch("/courses/:id/add-note", addNoteToCourse);
 
 export default courseRoute;
