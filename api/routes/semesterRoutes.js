962a8849f17ba1bf61ff82d0cf96dc5f729d0e4d
Henrique Barbosa <henriqbarbosa@icloud.com>
2021-02-01 11:39:24 -0300

    Hotfix 2 aula-semestre

--- api/routes/semesterRoutes.js
+++ api/routes/semesterRoutes.js
@@ -1,8 +1,7 @@
 import Router from 'express';
-import { createSemester, addClassesToSemester } from '../controllers/semester.js';
+import { createSemester } from '../controllers/semester.js';
 
 const semesterRoute = Router();
 semesterRoute.post("/semesters", createSemester);
-semesterRoute.patch("/semesters/:id/add-classes", addClassesToSemester);
 
 export default semesterRoute;
