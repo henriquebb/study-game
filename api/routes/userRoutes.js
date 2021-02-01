64810a55109d535c6041204283242b0cdac2dcb0
Henrique Barbosa <henriqbarbosa@icloud.com>
2021-02-01 16:34:19 -0300

    Editar usuario

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import Router from 'express';
-import { createUser, addItemToUser, getUser } from '../controllers/user.js';
+import { createUser, addItemToUser, getUser, editUser } from '../controllers/user.js';
 
 const userRoute = Router();
 userRoute.get('/users/:id', getUser);
 userRoute.post("/users", createUser);
 userRoute.patch("/users/:id/add-item", addItemToUser);
+userRoute.patch("/users/:id", editUser);
 
 export default userRoute;
