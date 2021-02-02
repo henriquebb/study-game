b2313ba1d131f8d7b04876fba841ec35c81d2bbb
Henrique Barbosa <henriqbarbosa@icloud.com>
2021-02-02 16:30:03 -0300

    Pegar info de semestre

--- api/routes/semesterRoutes.js
+++ api/routes/semesterRoutes.js
@@ -1,9 +1,10 @@
 import Router from 'express';
-import { createSemester, addNoteToSemester, editSemester } from '../controllers/semester.js';
+import { createSemester, addNoteToSemester, editSemester, getSemester } from '../controllers/semester.js';
 
 const semesterRoute = Router();
 semesterRoute.post("/semesters", createSemester);
 semesterRoute.patch("/semesters/:id", editSemester);
 semesterRoute.patch("/semesters/:id/add-note", addNoteToSemester);
+semesterRoute.get("/semesters/:id", getSemester);
 
 export default semesterRoute;
