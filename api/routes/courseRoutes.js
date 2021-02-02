193ef94a5034c45dee872cf14ab51238f4d4de1b
Henrique Barbosa <henriqbarbosa@icloud.com>
2021-02-02 12:04:55 -0300

    Editar aula

--- api/routes/courseRoutes.js
+++ api/routes/courseRoutes.js
@@ -1,9 +1,10 @@
 import Router from 'express';
-import { addNoteToCourse, createCourse, getCourse } from '../controllers/course.js';
+import { addNoteToCourse, createCourse, getCourse, editCourse } from '../controllers/course.js';
 
 const courseRoute = Router();
 courseRoute.post("/courses", createCourse);
 courseRoute.patch("/courses/:id/add-note", addNoteToCourse);
 courseRoute.get("/courses/:id", getCourse);
+courseRoute.patch("/courses/:id", editCourse);
 
 export default courseRoute;
